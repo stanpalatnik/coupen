71a73556aefcfeaf3134a92c909fd9aa5362c26f
Stanislav Palatnik <stan@spalatnik.com>
2015-05-08 15:25:16 -0400

    added angular-socket-io package

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('coupen', ['ionic', 'coupen.controllers', 'coupen-constants', 'coupen.directives.qrCode'])
+angular.module('coupen', ['ionic', 'coupen.controllers', 'coupen-constants', 'coupen.directives.qrCode', 'btford.socket-io'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -18,6 +18,10 @@ angular.module('coupen', ['ionic', 'coupen.controllers', 'coupen-constants', 'co
       StatusBar.styleDefault();
     }
   });
+})
+
+.factory('myWebSocket', function (socketFactory) {
+  return socketFactory();
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
